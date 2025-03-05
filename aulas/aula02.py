1b21527d8b6600f22c4178518d0d99c23cea8727
Eric Lopes da Silva <160548042+EricLopes325@users.noreply.github.com>
2025-03-05 13:25:49 -0300

    Aula feita até 04

---
+++ aulas/aula02.py
@@ -0,0 +1,11 @@
+
+""""
+SEP é um "separador"
+\r\n -> CRLF
+\n -> LF
+END = O final do "print"
+"""
+
+
+print(12,34, sep= "-", end="\r\n##")
+print(12,34, sep= " eric ", end="\r\n") 
---
+++ aulas/aula03.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Eric Lopes')
+print(1, 'Eric Lopes"')
+
+# Aspas duplas
+print("Eric Lopes")
+print(2, "Eric Lopes'")
+
+# Escape
+print("Eric \"Lopes\"")
+
+# r
+print(r"Luiz \"Otávio\"")
