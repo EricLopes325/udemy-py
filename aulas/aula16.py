307ebcffa34ba37e74cd49d420217814680703d9
Eric Lopes da Silva <160548042+EricLopes325@users.noreply.github.com>
2025-03-10 13:44:18 -0300

    finalizado da aula 15 até a 20

---
+++ aulas/aula16.py
@@ -0,0 +1,12 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input("voce quer 'entrar' ou 'sair' do sitema: ")
+
+if entrada == "entrar":
+    print("voce entou no sistema!!")
+
+elif entrada == "sair":
+    print("voce saiu do sistema!!")
+
+else:
+    print("voce nao digitou nem 'entrar' e nem 'sair'. Presiso saber!!")
